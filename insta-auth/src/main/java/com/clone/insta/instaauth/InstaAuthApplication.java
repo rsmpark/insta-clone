b6ab986e21a56f451cc5a88214ef4ff1b14d9df5
rsmpark <rick.sm.park@gmail.com>
2023-03-11 21:02:12 -0500

    Add init auth repo

---
+++ insta-auth/src/main/java/com/clone/insta/instaauth/InstaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaAuthApplication.class, args);
+    }
+
+}
---
+++ insta-auth/src/test/java/com/clone/insta/instaauth/InstaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instaauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
