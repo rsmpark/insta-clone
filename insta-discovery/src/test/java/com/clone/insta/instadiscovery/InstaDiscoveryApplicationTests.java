45ae09d118f8b29c96b6253db3628e08c46169c8
rsmpark <rick.sm.park@gmail.com>
2023-03-11 20:45:27 -0500

    Adding service discovery

---
+++ insta-discovery/src/test/java/com/clone/insta/instadiscovery/InstaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instadiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
