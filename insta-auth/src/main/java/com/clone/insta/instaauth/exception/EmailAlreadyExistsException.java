acc3e2f2231b239bf43bf63000d103f580993428
SangMin <9714park@gmail.com>
2023-03-13 15:58:13 -0400

    Add user service

---
+++ insta-auth/src/main/java/com/clone/insta/instaauth/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.clone.insta.instaauth.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
