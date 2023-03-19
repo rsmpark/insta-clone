56c15707b5031d7571ccd14e8c9e0df27398b138
rsmpark <rick.sm.park@gmail.com>
2023-03-19 11:21:06 -0400

    Add user auditor to set username into ImageMetadata model from securityContext.

---
+++ insta-media/src/main/java/com/clone/insta/instamedia/config/UserAudior.java
@@ -0,0 +1,18 @@
+package com.clone.insta.instamedia.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserAudior implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return Optional.of(username);
+    }
+}
