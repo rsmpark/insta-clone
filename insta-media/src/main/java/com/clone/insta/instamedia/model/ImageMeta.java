8740dbdc6f6d98b27464508f1c10bcf87bafae1c
rsmpark <rick.sm.park@gmail.com>
2023-03-19 10:40:44 -0400

    Add ImageMeta model.

---
+++ insta-media/src/main/java/com/clone/insta/instamedia/model/ImageMeta.java
@@ -0,0 +1,35 @@
+package com.clone.insta.instamedia.model;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Data
+@RequiredArgsConstructor
+@Document
+public class ImageMeta {
+
+    @Id
+    private String id;
+
+    @CreatedBy
+    private String username;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @NonNull
+    private String filename;
+
+    @NonNull
+    private String uri;
+
+    @NonNull
+    private String fileType;
+}
