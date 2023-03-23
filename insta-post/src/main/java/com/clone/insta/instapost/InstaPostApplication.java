70c54a3123f0714928dafd23106250683886c801
rsmpark <rick.sm.park@gmail.com>
2023-03-22 21:15:19 -0400

    Add initial module insta posts

---
+++ insta-post/src/main/java/com/clone/insta/instapost/InstaPostApplication.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaPostApplication.class, args);
+	}
+
+}
---
+++ insta-post/src/test/java/com/clone/insta/instapost/InstaPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instapost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
