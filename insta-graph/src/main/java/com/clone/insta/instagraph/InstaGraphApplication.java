235bc2ecc6519cbcd74ee2f203b5333e3d749860
rsmpark <rick.sm.park@gmail.com>
2023-03-25 19:57:09 -0400

    Add initial insta-graph repo.

---
+++ insta-graph/src/main/java/com/clone/insta/instagraph/InstaGraphApplication.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instagraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaGraphApplication.class, args);
+    }
+
+}
---
+++ insta-graph/src/test/java/com/clone/insta/instagraph/InstaGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instagraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
