b9aaf789c85a4b81d6993b5e5eaa979a3051d2c2
rsmpark <rick.sm.park@gmail.com>
2023-03-28 07:12:43 -0400

    Add user graph service.

---
+++ insta-graph/src/main/java/com/clone/insta/instagraph/exception/UsernameNotExistsException.java
@@ -0,0 +1,12 @@
+package com.clone.insta.instagraph.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UsernameNotExistsException extends RuntimeException {
+
+    public UsernameNotExistsException(String message) {
+        super(message);
+    }
+}
