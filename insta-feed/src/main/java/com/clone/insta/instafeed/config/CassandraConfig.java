f9b7c3c677b73da91dacbbd59037742d3cac15c9
SangMin <9714park@gmail.com>
2023-03-30 14:31:31 -0400

    Add cassandra config.

---
+++ insta-feed/src/main/java/com/clone/insta/instafeed/config/CassandraConfig.java
@@ -0,0 +1,77 @@
+package com.clone.insta.instafeed.config;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.SessionFactory;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories(basePackages = "com.clone.insta.instafeed.repository")
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspaceName}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.contactPoints}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.port}")
+    private int port;
+
+    @Bean
+    public CqlSessionFactoryBean session() {
+
+        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
+        session.setContactPoints(contactPoints);
+        session.setKeyspaceName(keyspace);
+
+        return session;
+    }
+
+    @Bean
+    public SessionFactoryFactoryBean sessionFactory(CqlSession session, CassandraConverter converter) {
+
+        SessionFactoryFactoryBean sessionFactory = new SessionFactoryFactoryBean();
+        sessionFactory.setSession(session);
+        sessionFactory.setConverter(converter);
+        sessionFactory.setSchemaAction(SchemaAction.NONE);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public CassandraMappingContext mappingContext(CqlSession cqlSession) {
+
+        CassandraMappingContext mappingContext = new CassandraMappingContext();
+        mappingContext.setUserTypeResolver(new SimpleUserTypeResolver(cqlSession));
+
+        return mappingContext;
+    }
+
+    @Bean
+    public CassandraConverter converter(CassandraMappingContext mappingContext) {
+        return new MappingCassandraConverter(mappingContext);
+    }
+
+    @Bean
+    public CassandraOperations cassandraTemplate(SessionFactory sessionFactory, CassandraConverter converter) {
+        return new CassandraTemplate(sessionFactory, converter);
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+}
