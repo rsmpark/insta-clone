3ef958593ca6770e38f3fa62257698259c5540cb
SangMin <9714park@gmail.com>
2023-03-30 14:16:42 -0400

    Add initial repo for insta-feed module.

---
+++ insta-feed/src/main/java/com/clone/insta/instafeed/InstaFeedApplication.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instafeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaFeedApplication.class, args);
+    }
+
+}
---
+++ insta-feed/src/test/java/com/clone/insta/instafeed/InstaFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instafeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
