a3f0cd3deb590cf50d8f9eb536f1c068b6c57cee
SangMin <9714park@gmail.com>
2023-03-31 11:18:11 -0400

    Add configs for cassandra.

--- insta-feed/src/main/java/com/clone/insta/instafeed/config/CassandraConfig.java
+++ insta-feed/src/main/java/com/clone/insta/instafeed/config/CassandraConfig.java
@@ -13,9 +13,14 @@ import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.data.cassandra.core.convert.CassandraConverter;
 import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableCassandraRepositories(basePackages = "com.clone.insta.instafeed.repository")
@@ -30,12 +35,19 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Value("${spring.data.cassandra.port}")
     private int port;
 
+    @Value("${spring.data.cassandra.schema-action}")
+    private SchemaAction schemaAction;
+
+
+
     @Bean
     public CqlSessionFactoryBean session() {
 
         CqlSessionFactoryBean session = new CqlSessionFactoryBean();
         session.setContactPoints(contactPoints);
         session.setKeyspaceName(keyspace);
+        session.setPort(port);
+        session.setKeyspaceCreations(getKeyspaceCreations());
 
         return session;
     }
@@ -46,11 +58,10 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         SessionFactoryFactoryBean sessionFactory = new SessionFactoryFactoryBean();
         sessionFactory.setSession(session);
         sessionFactory.setConverter(converter);
-        sessionFactory.setSchemaAction(SchemaAction.NONE);
+        sessionFactory.setSchemaAction(schemaAction);
 
         return sessionFactory;
     }
-
     @Bean
     public CassandraMappingContext mappingContext(CqlSession cqlSession) {
 
@@ -73,5 +84,16 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Override
     protected String getKeyspaceName() {
         return keyspace;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.
+                createKeyspace(keyspace)
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication();
+
+        return Arrays.asList(specification);
     }
 }
