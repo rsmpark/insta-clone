9b4cde362949846818d230178d442df9dec3c9cd
SangMin <9714park@gmail.com>
2023-04-03 09:55:13 -0400

    Add kafka binding.

--- insta-feed/src/main/java/com/clone/insta/instafeed/InstaFeedApplication.java
+++ insta-feed/src/main/java/com/clone/insta/instafeed/InstaFeedApplication.java
@@ -1,10 +1,13 @@
 package com.clone.insta.instafeed;
 
+import com.clone.insta.instafeed.messaging.PostEventStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
+@EnableBinding(PostEventStream.class)
 @EnableFeignClients
 public class InstaFeedApplication {
     public static void main(String[] args) {
