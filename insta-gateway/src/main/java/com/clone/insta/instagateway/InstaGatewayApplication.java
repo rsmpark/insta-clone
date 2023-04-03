8acc9ea25ae26e7cbbe97a2d91eb47536e0ba4ca
SangMin <9714park@gmail.com>
2023-04-03 11:13:08 -0400

    Add initial insta-gateway repo.

---
+++ insta-gateway/src/main/java/com/clone/insta/instagateway/InstaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaGatewayApplication.class, args);
+	}
+
+}
---
+++ insta-gateway/src/test/java/com/clone/insta/instagateway/InstaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.insta.instagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
