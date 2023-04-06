def7bb98c6e792e473752d92b069134d67c0d799
SangMin <9714park@gmail.com>
2023-04-06 10:06:24 -0400

    Add app header.

---
+++ insta-front/src/common/AppHeader.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+import { Menu, Row, Col, Input, Icon } from "antd";
+import { Link, withRouter } from "react-router-dom";
+
+import "./AppHeader.css";
+const Search = Input.Search;
+
+class AppHeader extends Component {
+    state = {};
+
+    render () {
+        const menuItems = [
+            <Menu.Item title="Find friends" key="discover">
+                <Link to="/discover">
+                    <Icon type="compass" />
+                </Link>
+            </Menu.Item>,
+            <Menu.Item key="notification">
+                <Icon type="heart" />
+            </Menu.Item>,
+            <Menu.Item key="userProfile">
+                <Link to={"/users/me"}>
+                    <Icon type="user" />
+                </Link>
+            </Menu.Item>
+        ];
+
+        return (
+            <div style={{ marginLeft: "17%", marginRight: "17%" }}>
+                <Row>
+                    <Col span={8}>
+                        <div className="app-logo-container">
+                            <Link to="/">
+                                <span>𝓜𝔂 𝓜𝓸𝓶𝓮𝓷𝓽𝓼</span>
+                            </Link>
+                        </div>
+                    </Col>
+
+                    <Col span={6}>
+                        <Search />
+                    </Col>
+
+                    <Col span={8} push={4}>
+                        <Menu
+                            mode="horizontal"
+                            className="app-menu"
+                            selectable={false}
+                            onClick={this.handleMenuItemClick}
+                        >
+                            {menuItems}
+                        </Menu>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default withRouter(AppHeader);
